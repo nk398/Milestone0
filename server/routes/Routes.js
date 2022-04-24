03c3809e6a8c6e789754a743f84b495397c097be
Neha Kazi <47070642+nk398@users.noreply.github.com>
2022-04-24 12:29:33 -0400

    Create Routes.js

---
+++ server/routes/Routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const Controller = require('../controllers/Controller');
+
+/**
+ * App Routes 
+*/
+router.get('/', Controller.homepage);
+
+router.get('/submit-Thoughts', Controller.submitThoughts);
+router.post('/submit-Thoughts', Controller.submitThoughtsOnPost);
+
+
+ 
+module.exports = router;
