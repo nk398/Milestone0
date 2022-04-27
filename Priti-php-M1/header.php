979bee67029c828faac30c715c80a69d9b8f68b1
Priti <99843747+Priti222@users.noreply.github.com>
2022-04-27 02:35:12 -0400

    Delete Priti-php-M1 directory

--- Priti-php-M1/header.php
+++
@@ -1,3 +0,0 @@
-<div class="w3-container w3-text-red">
-  <h6 class="w3-center "><b>New Jersey Institute of Technology Highlanders</b></h6>
-</div>
--- Priti-php-M1/sponsors.php
+++
@@ -1,3 +0,0 @@
-<div class="w3-card" style="width: 100%;">
-    <img width="100%" src="sponsors/sponsors.png">
-</div>
